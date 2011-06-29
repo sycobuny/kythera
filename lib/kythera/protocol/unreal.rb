78fe966d8fb7a322c78f830886073f35563b12a0
Eric Will <rakaur@malkier.net>
2011-06-29 13:45:13 -0500

    Minor style changes

--- lib/kythera/protocol/unreal.rb
+++ lib/kythera/protocol/unreal.rb
@@ -40,7 +40,8 @@ module Protocol::Unreal
     # @param [String] real user's realname / gecos
     #
     def introduce_user(nick, user, host, real, modes = '')
-        modes = "+S#{modes}" # Services protection + introduce_user#modes
+        # Always mark our pseudoclients as services bots
+        modes = "+S#{modes}"
 
         u = send_nick(nick, user, host, real)
         u.parse_modes(modes)
